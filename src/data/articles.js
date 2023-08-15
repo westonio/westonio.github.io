aa3add367abf6754afda6762084fa930bda6d0c8
Weston Schutt <wtschutt@gmail.com>
2023-08-14 22:19:02 -0600

    Update article info

--- src/data/articles.js
+++ src/data/articles.js
@@ -4,10 +4,10 @@ const myArticles =  {
 	all: [
 		{
 			date: "14 August 2023",
-			title: "How Studying Chinese Helped Me Transition to Tech",
+			title: "How Studying Mandarin Helped Me Transition to Tech",
 			description:
-				"Learning a foreign language like Mandarin Chinese can provide you with valuable and transferrable skills that can enhance performance and effectiveness in tech roles.",
-			link: "https://medium.com/@westonio-dev/how-studying-chinese-helped-me-transition-to-tech-13d9c06fe439",
+				"Learning a foreign language like Mandarin can provide you with valuable and transferrable skills that can enhance performance and effectiveness in tech roles.",
+			link: "https://medium.com/@westonio-dev/how-studying-mandarin-helped-me-transition-to-tech-13d9c06fe439",
 		},
 	],
 
