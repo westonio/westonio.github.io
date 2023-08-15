1944db810cc0e447a53237ede870d7e45c325b19
Weston Schutt <wtschutt@gmail.com>
2023-08-14 22:01:38 -0600

    Remove tracking

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ import Articles from "./pages/articles";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
-
-function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
 
 	return (
 		<div className="App">
